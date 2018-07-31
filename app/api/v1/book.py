e8f3ca8237bc36727c1d77ff3c97d08f8b52640a
huangkai <18778335525@163.com>
2018-07-31 23:17:33 +0800

    蓝图的试用

---
+++ app/api/v1/book.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+
+
+book = Blueprint('book', __name__)
+
+
+@book.route('/v1/book/get')
+def get_book():
+    return 'get book'
--- app/app.py
+++ app/app.py
@@ -1,10 +1,26 @@
 from flask import Flask
 
 
+def register_blueprints(app):
+    """
+    注册蓝图
+    :param app:
+    :return:
+    """
+    from app.api.v1.user import user
+    from app.api.v1.book import book
+    app.register_blueprint(user)
+    app.register_blueprint(book)
+
+
 def create_app():
     # __name__为配置信息
     app = Flask(__name__)
     # 导入配置项
     app.config.from_object('app.config.setting')
     app.config.from_object('app.config.secure')
+
+    # 调用注册蓝图的函数
+    register_blueprints(app)
+
     return app
