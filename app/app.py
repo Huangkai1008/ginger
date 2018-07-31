2c4a6c97cf544cd0b665de59b52081fd8a2e2dac
huangkai <18778335525@163.com>
2018-07-31 22:56:37 +0800

    配置好flask入口文件以及config

--- app/app.py
+++ app/app.py
@@ -2,5 +2,9 @@ from flask import Flask
 
 
 def create_app():
-    app = Flask()
-    pass
+    # __name__为配置信息
+    app = Flask(__name__)
+    # 导入配置项
+    app.config.from_object('app.config.setting')
+    app.config.from_object('app.config.secure')
+    return app
