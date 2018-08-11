6a6ea0385f2a6b0b3166d1b33f8b98cea606a636
huangkai <18778335525@163.com>
2018-08-11 15:56:07 +0800

    重写wt_form

---
+++ app/validators/base.py
@@ -0,0 +1,14 @@
+from wtforms import Form
+
+from app.libs.error_code import ParameterException
+
+
+class BaseForm(Form):
+    def __init__(self, data):
+        super(BaseForm, self).__init__(data=data)
+
+    def validate_for_api(self):
+        valid = super(BaseForm, self).validate()
+        if not valid:
+            # form errors
+            raise ParameterException(msg=self.errors)
