357c48c60b4a1f4fce56efec059189b8caeaf745
huangkai <18778335525@163.com>
2018-08-25 21:45:41 +0800

    重写BaseQuery的get_or_404()

--- app/models/user.py
+++ app/models/user.py
@@ -37,9 +37,7 @@ class User(Base):
         :param password:
         :return:
         """
-        user = User.query.filter_by(email=email).first()
-        if not user:
-            raise NotFound(msg='user not found')
+        user = User.query.filter_by(email=email).first_or_404()
         if not user.check_password(password):
             raise AuthFailed()
         return {'uid': user.id}
@@ -48,5 +46,6 @@ class User(Base):
         if not self._password:
             return False
         return check_password_hash(self._password, raw)
+    
 
 
